3d3f1c7d3ae24a0241e000172b652434e8ae10d5
Hypertera <hypertera2@gmail.com>
2026-06-29 17:58:25 -0600

    Add files via upload

---
+++ Pruebas_INO/Prueba_Motores.ino
@@ -0,0 +1,35 @@
+#include <AccelStepper.h>
+
+// Pines CNC Shield v3
+const int stepX = 2; const int dirX = 5;
+const int stepY = 3; const int dirY = 6;
+const int enablePin = 8;
+
+AccelStepper motorX(1, stepX, dirX);
+AccelStepper motorY(1, stepY, dirY);
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(enablePin, OUTPUT);
+  digitalWrite(enablePin, LOW); // Habilitar motores
+
+  motorX.setMaxSpeed(4000); // Límite máximo de pulsos/seg
+  motorY.setMaxSpeed(4000);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    char c = Serial.read();
+    if (c == 'v') {
+      long p_x = Serial.parseInt();
+      long p_y = Serial.parseInt();
+      
+      motorX.setSpeed(p_x);
+      motorY.setSpeed(p_y);
+    }
+  }
+  
+  // Estas funciones deben ejecutarse lo más rápido posible
+  motorX.runSpeed();
+  motorY.runSpeed();
+}
